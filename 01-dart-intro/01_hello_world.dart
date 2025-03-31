01f04ebd8a324c65600296cf7c5dca64eb944fc1
Gilbert Acosta <gisawebdev@gmail.com>
2025-03-31 06:52:56 -0400

    update

---
+++ 01-dart-intro/01_hello_world.dart
@@ -0,0 +1,10 @@
+void main() {
+  // var myName = 'Gilbert';
+  // late final myName;
+  // String myName = 'Gilbert';
+  const myName = 'Gilbert';
+
+  print('Hello $myName');
+  print('Hello ${myName.toUpperCase()}');
+  print('Hello ${1 + 1}');
+}
